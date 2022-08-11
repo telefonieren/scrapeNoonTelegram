14837165288d55d9839f17d1db131758aa02d465
telefonieren <alex.telefonieren@gmail.com>
2022-08-11 09:11:33 +0300

    done

--- discount_bot.py
+++ discount_bot.py
@@ -1,26 +1,66 @@
 import time
 import json
 
+
 from aiogram.utils.markdown import hbold, hlink, hitalic
 
+#
+#
+# bot = Bot(token='5344108083:AAFuOcFEprEmS_Ew7Hery7JKNMwjQTrH7F8')
+# dp = Dispatcher(bot)
+# c_id = 0
+#
+# @dp.message_handler(commands=['start','help'])
+# async def start(message: types.Message):
+#
+#     await message.answer(f'Привет! Я бот noon.com id: {message.chat.id}')
+#     # while True:
+#         with open('final_result.json') as file:
+#             data = json.load(file)
+#             for item in data:
+#                 card = f"{hlink(item.get('title'),item.get('link'))}\n" \
+#                     f"{hbold('Цена: ')} {hbold(item.get('new_price'))} 🔥\n" \
+#                     f"Старая цена: {item.get('old_price')}\n" \
+#                     f"{hbold('Общая скидка: ')}{hitalic(item.get('discount'))} 😍"
+#                 print(c_id['chat']['id'])
+#                 bot.send_message(c_id['chat']['id'], card, parse_mode=types.ParseMode.HTML)
+#                 time.sleep(10)
+#         file.close()
+#
+#
+#
+# def main():
+#
+#     executor.start_polling(dp, skip_updates=True)
+#
+#
+#
+# if __name__ == '__main__':
+#     main()
 import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
+
 API_TOKEN = '5344108083:AAFuOcFEprEmS_Ew7Hery7JKNMwjQTrH7F8'
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+
 bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
+
 
 
 async def send_message(channel_id: int, text: str):
     await bot.send_message(channel_id, text)
 
-
 async def main():
+
+
+
     with open('final_result.json') as file:
         data = json.load(file)
         for item in data:
@@ -28,11 +68,15 @@ async def main():
                    f"{hbold('Цена: ')} {hbold(item.get('new_price'))} 🔥\n" \
                    f"Старая цена: {item.get('old_price')}\n" \
                    f"{hbold('Общая скидка: ')}{hitalic(item.get('discount'))} 😍\n" \
- \
+
+
             await bot.send_message(-1001712092516, card, parse_mode=types.ParseMode.HTML)
             print(card)
             time.sleep(1400)
     file.close()
+
+
+
 
 
 if __name__ == '__main__':
